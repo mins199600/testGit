d251f590a0ce0538c00c8d722babfc1efa4bb08d
mins199600 <153487560+mins199600@users.noreply.github.com>
2023-12-14 15:07:03 +0900

    init

--- src/com/ohgiraffers/test/GitApp.java
+++ src/com/ohgiraffers/test/GitApp.java
@@ -3,6 +3,6 @@ package com.ohgiraffers.test;
 public class GitApp {
 
     public static void main(String[] args) {
-        System.out.println("오늘 점심 로제게살리조뜨~! 야채빼고!! 오예");
+        int a= 1;
     }
 }
